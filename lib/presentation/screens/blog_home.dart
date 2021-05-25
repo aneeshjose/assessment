614c74c202345e6c8c80b9712e0791718f41b356
Aneesh Jose <31209870+aneeshjose@users.noreply.github.com>
2021-05-25 14:05:17 +0530

    appBar created

--- lib/presentation/screens/blog_home.dart
+++ lib/presentation/screens/blog_home.dart
@@ -4,6 +4,24 @@ class BlogHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: InkWell(
+          child: Icon(
+            Icons.home,
+            color: Colors.black,
+            size: 35,
+          ),
+        ),
+        title: Text(
+          'BluePad',
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 25,
+          ),
+        ),
+      ),
       body: Center(
         child: Text(''),
       ),
